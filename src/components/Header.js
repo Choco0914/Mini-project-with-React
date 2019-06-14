59570b58ca4a5ceedd94a55117d2e9c5503a8b97
Choco0914 <heojeongho1992@gmail.com>
2019-06-14 20:18:08 +0900

    홈페이지 타이틀 컴포넌트 만들기

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomeTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 10px;
+  color: #ecf0f1;
+  background-color: #2f3640;
+`;
+const MenuButton = styled.i`
+  position: absolute;
+  left: 2%;
+  cursor: pointer;
+`;
+const Title = styled.span``;
+
+const Header = () => {
+  return (
+    <HomeTitle>
+      <MenuButton className="fas fa-bars" />
+      <Title>허정호님의 홈페이지</Title>
+    </HomeTitle>
+  );
+};
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./Header";
 
 const HomeContainer = styled.div``;
 
 const Home = () => {
-  return <HomeContainer>Home</HomeContainer>;
+  return (
+    <HomeContainer>
+      <Header />
+      home
+    </HomeContainer>
+  );
 };
 
 export default Home;
